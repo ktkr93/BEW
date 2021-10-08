76be07e1615ae4c325d0d566a3959eb3478747cb
ktkr93 <ktkr93@gmail.com>
2021-10-08 15:00:16 +0900

    modify fadeInに関するCSS、JSの記述を修正

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ function fadeAnime() {
 
     //ふわっと動くきっかけのクラス名と動きのクラス名の設定
     $('.fadeInTrigger').each(function() { //fadeInUpTriggerというクラス名が
-        var elemPos = $(this).offset().top - 50; //要素より、50px上の
+        var elemPos = $(this).offset().top - 20; //要素より、30px上の
         var scroll = $(window).scrollTop();
         var windowHeight = $(window).height();
         if (scroll >= elemPos - windowHeight) {
             $(this).addClass('fadeIn'); // 画面内に入ったらfadeInというクラス名を追記
+        } else {
+            $(this).removeClass('fadeIn'); // 画面外に出たらfadeInというクラス名を外す
         }
+
     });
 
 } //ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
